0a76b074d4942d231432eefdc9eea56df050214f
JoseG1297 <mordo1309@gmail.com>
2025-02-20 13:00:11 -0600

    primeras rutas

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { ProductFormComponent } from './components/product-form/product-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+    path: '',
+    component: ProductListComponent
+},
+{
+    path: 'products',
+    component: ProductListComponent
+},
+{
+    path: 'products/create/:id',
+    component: ProductFormComponent
+},
+{
+    path: 'products/edit/:id',
+    component: ProductFormComponent
+}
+];
+
+@NgModule({
+    imports: [],
+    exports: []
+})
+export class AppRoutesModule { }
