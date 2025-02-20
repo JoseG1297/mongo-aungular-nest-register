980fcea3d9cff26777e05c7da389207fae08c5d9
JoseG1297 <mordo1309@gmail.com>
2025-02-20 15:51:48 -0600

    nueva version de front

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,29 +1,3 @@
-import { Routes, RouterModule } from '@angular/router';
-import { NgModule } from '@angular/core';
+import { Routes } from '@angular/router';
 
-import { ProductListComponent } from './components/product-list/product-list.component';
-import { ProductFormComponent } from './components/product-form/product-form.component';
-
-export const routes: Routes = [{
-    path: '',
-    component: ProductListComponent
-},
-{
-    path: 'products',
-    component: ProductListComponent
-},
-{
-    path: 'products/create',
-    component: ProductFormComponent
-},
-{
-    path: 'products/edit/:id',
-    component: ProductFormComponent
-},
-];
-
-@NgModule({
-    imports: [RouterModule.forRoot(routes)],
-    exports: [RouterModule]
-})
-export class AppRoutesModule { }
+export const routes: Routes = [];
