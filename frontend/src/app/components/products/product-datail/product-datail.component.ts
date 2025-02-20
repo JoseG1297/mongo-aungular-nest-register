244d294384c1b019b43cbafe8b4a3a74e0e49dd2
JoseG1297 <mordo1309@gmail.com>
2025-02-20 16:47:31 -0600

    nueva estructura productos

---
+++ frontend/src/app/components/products/product-datail/product-datail.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-datail',
+  imports: [],
+  templateUrl: './product-datail.component.html',
+  styles: ``
+})
+export class ProductDatailComponent {
+
+}
