7ecd47f8a65c69bdee95a380b29312cb7365fbc1
JoseG1297 <mordo1309@gmail.com>
2025-02-21 17:01:26 -0600

    controller methods products

--- backend/src/products/products.controller.ts
+++ backend/src/products/products.controller.ts
@@ -1,6 +1,6 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Put, Delete, Param } from '@nestjs/common';
 import { ProductsService } from './products.service';
-import { CreateProductsDto } from './dto/products.dto';
+import { CreateProductsDto, UpdateProductsDto  } from './dto/products.dto';
 import { Product } from './schema/product.schema';
 
 @Controller('products')
@@ -8,9 +8,31 @@ export class ProductsController {
 
     constructor(private readonly productsService: ProductsService) { }
 
-    @Post('product')
+    @Post()
     async createInvoice(@Body() createProductsDto:CreateProductsDto):Promise<Product>{
         return await this.productsService.createProduct(createProductsDto);
     }
 
+    @Get()
+    async findAll(): Promise<Product[]> {
+        return this.productsService.getAllProducts();
+    }
+
+    @Get(':id')
+    async findOne(@Param('id') id: string): Promise<Product> {
+        return this.productsService.getProductById(id);
+    }
+
+    @Put(':id')
+    async update(
+        @Param('id') id: string,
+        @Body() updateProductsDto: UpdateProductsDto,
+    ): Promise<Product> {
+        return this.productsService.updateProduct(id, updateProductsDto);
+    }
+
+    @Delete(':id')
+    async remove(@Param('id') id: string): Promise<{ message: string }> {
+        return this.productsService.deleteProduct(id);
+    }
 }
