b7bc49d6696d745854811925bdf4e6e5f546b651
JoseG1297 <mordo1309@gmail.com>
2025-02-21 17:34:09 -0600

    tratando de consumir el servicio getProducts

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 import { HeaderComponent } from './shared/ui/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- frontend/src/app/components/products/product-list/product-list.component.ts
+++ frontend/src/app/components/products/product-list/product-list.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductsService } from '../../../services/products.service';
+import { Product } from '../../../services/models/product.model';
 
 @Component({
   selector: 'app-product-list',
-  imports: [],
   templateUrl: './product-list.component.html',
-  styles: ``
+  styleUrls: ['./product-list.component.scss']
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
+  products: Product[] = [];
 
+  constructor(private productsService: ProductsService) {}
+
+  ngOnInit(): void {
+    this.productsService.getProducts().subscribe((data: any) => {
+      this.products = data;
+    });
+  }
 }
