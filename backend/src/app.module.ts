3a2160bf15ab78efe1c9bc689b24eab799b00b96
JoseG1297 <mordo1309@gmail.com>
2025-02-21 16:53:43 -0600

    se estaba llamando dos veces el products module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from './database/database.module';
 import { ProductsModule } from './products/products.module';
-import { ProductsModule } from './products/products.module';
 
 @Module({
   imports: [DatabaseModule, ProductsModule],
