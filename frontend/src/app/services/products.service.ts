3b80cb9db9fcb4342eaa5019f91aa282f22ea41f
JoseG1297 <mordo1309@gmail.com>
2025-02-25 11:37:41 -0600

    base http host

--- frontend/src/app/services/products.service.ts
+++ frontend/src/app/services/products.service.ts
@@ -1,33 +1,32 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Product } from './models/product.model'; // Define un modelo de producto
+import { BaseHttpService } from './http/base-http.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ProductsService {
-  private apiUrl = 'http://localhost:3000/products'; // URL del backend
+export class ProductsService extends BaseHttpService{
 
-  constructor(private http: HttpClient) {}
+  private newUrl = this.apiUrl + "/products";
 
   getProducts(): Observable<Product[]> {
-    return this.http.get<Product[]>(this.apiUrl);
+    return this.http.get<Product[]>(this. newUrl);
   }
 
   getProduct(id: string): Observable<Product> {
-    return this.http.get<Product>(`${this.apiUrl}/${id}`);
+    return this.http.get<Product>(`${this. newUrl}/${id}`);
   }
 
   createProduct(product: Product): Observable<Product> {
-    return this.http.post<Product>(this.apiUrl, product);
+    return this.http.post<Product>(this. newUrl, product);
   }
 
   updateProduct(id: string, product: Product): Observable<Product> {
-    return this.http.put<Product>(`${this.apiUrl}/${id}`, product);
+    return this.http.put<Product>(`${this. newUrl}/${id}`, product);
   }
 
   deleteProduct(id: string): Observable<{ message: string }> {
-    return this.http.delete<{ message: string }>(`${this.apiUrl}/${id}`);
+    return this.http.delete<{ message: string }>(`${this. newUrl}/${id}`);
   }
 }
