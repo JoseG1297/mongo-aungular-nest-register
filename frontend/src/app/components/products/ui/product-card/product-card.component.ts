a893c0a45b30b80ec8e972e211e6bf23e3dd64e4
JoseG1297 <mordo1309@gmail.com>
2025-02-26 11:53:50 -0600

    primeros estilos opara listado de productos

--- frontend/src/app/components/products/ui/product-card/product-card.component.ts
+++ frontend/src/app/components/products/ui/product-card/product-card.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { Product } from '../../../../services/models/product.model';
 
 @Component({
   selector: 'app-product-card',
@@ -7,5 +8,5 @@ import { Component } from '@angular/core';
   styles: ``
 })
 export class ProductCardComponent {
-
+  product = input.required<Product>();
 }
