40cc21cb3072365e7454e114a8e5782cbd98558e
JoseG1297 <mordo1309@gmail.com>
2025-02-26 10:10:11 -0600

    componente para cards products

--- frontend/src/app/components/products/product-list/product-list.component.ts
+++ frontend/src/app/components/products/product-list/product-list.component.ts
@@ -1,11 +1,12 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common'; // 🔥 Necesario para *
 import { ProductStateService } from '../../../states/product-state.service';
+import { ProductCardComponent } from '../ui/product-card/product-card.component';
 
 @Component({
   selector: 'app-product-list',
   standalone: true, // ⬅️ Indica que el componente es independiente
-  imports: [CommonModule], // ⬅️ Agrega CommonModule aquí
+  imports: [CommonModule, ProductCardComponent], // ⬅️ Agrega CommonModule aquí
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.scss']
 })
